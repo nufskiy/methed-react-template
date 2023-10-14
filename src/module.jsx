b7a346035cf64b109849266f72f14a94fcd8d3f0
nufskiy <nufskiy@rambler.ru>
2023-10-15 01:34:19 +0300

    react app template via webpack

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import maks from './img/img.png';
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img width={500} src={maks} alt="Image" />
+    </>
+);
+
+export default Module;
